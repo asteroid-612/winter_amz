eaa5cb811760f09fc9f2d063dd34e1bb356c6f5d
Sujeong Lee <ekfqlc0001@kookmin.ac.kr>
2018-12-27 15:00:41 +0900

    according to data asin in page 1, crawled feature bullets

--- spiders/AmazonProductSpider.py
+++ spiders/AmazonProductSpider.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json 
 from winter_amz.items import WinterAmzItem
+
+with open('dataasins.json') as data_file:
+    data = json.load(data_file)
 
 class AmazonProductsSpider(scrapy.Spider):
     name = "AmazonItems"
     allowed_domains = ["amazon.com"]
-    start_urls = [
-        "https://www.amazon.com/dp/B07BKSR1SG", "https://www.amazon.com/dp/B07JKD2378",
-        "https://www.amazon.com/dp/B07K4Z7CGB", "https://www.amazon.com/dp/B07KPVSCDS"
-        ]
+    data_asins = data[0]["product_data_asin"].split(',') # product code list
+    for j in range(len(data_asins)):
+        data_asins[j] = 'https://www.amazon.com/dp/' + data_asins[j]
+##    start_urls = [
+##        "https://www.amazon.com/dp/B07BKSR1SG", "https://www.amazon.com/dp/B07JKD2378",
+##        "https://www.amazon.com/dp/B07K4Z7CGB", "https://www.amazon.com/dp/B07KPVSCDS"
+##        ]
     # want to do : start_urls from amazon.com, can't we just extract it from that url?
     # start_urls is initialized in empty form, and the process gets the item code "XXXXXXXXXX"(10digits) from amazon.com
-    
+
+    def start_requests(self):
+        for i in range(len(AmazonProductsSpider.data_asins)):
+            yield scrapy.Request(AmazonProductsSpider.data_asins[i], self.parse)
 
     def parse(self, response):
         items = WinterAmzItem()
